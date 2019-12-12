7f4a6871120183aea3ef3bb70edf7fd442bbaad4
CoreRasurae <luis.p.mendes@gmail.com>
2019-12-12 21:49:57 +0000

    Inserted license information to all remaining files

--- functionsLib/createPathNameForTestAndConditions.m
+++ functionsLib/createPathNameForTestAndConditions.m
@@ -1,3 +1,23 @@
+%This program generates synthetic PIV images and exports validation data.
+%Copyright (C) 2019  Luís Mendes, Prof. Rui Ferreira, Prof. Alexandre Bernardino
+%
+%This program is free software; you can redistribute it and/or
+%modify it under the terms of the GNU General Public License
+%as published by the Free Software Foundation; either version 2
+%of the License, or (at your option) any later version.
+%
+%This program is distributed in the hope that it will be useful,
+%but WITHOUT ANY WARRANTY; without even the implied warranty of
+%MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%GNU General Public License for more details.
+%
+%You should have received a copy of the GNU General Public License
+%along with this program; if not, write to the Free Software
+%Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+%
+%Special thanks go to Ana Margarida and Rui Aleixo
+%and their initial effort on building a draft for a similar tool.
+
 function [ pathName ] = createPathNameForTestAndConditions( flowParameters, pivParameters )
 %Helper function for defining output path name
 %   Detailed explanation goes here
