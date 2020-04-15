636fe167515436eef4dbcd2e3f6926faee5b1083
CoreRasurae <luis.p.mendes@gmail.com>
2020-04-15 19:47:52 +0100

    RankineVortexAndUniformFlow: Generalize parameterization to allow definition of the constant velocity u,v components from a ratio of max. velocity magnitude

--- src/MATLAB/functionsLib/RankineVortexAndUniformFlow.m
+++ src/MATLAB/functionsLib/RankineVortexAndUniformFlow.m
@@ -30,6 +30,9 @@ classdef RankineVortexAndUniformFlow
         dt
         u
         v
+        c
+        c1
+	M
         Weight = 1.0;
         Radius = 100;
     end
@@ -40,17 +43,23 @@ classdef RankineVortexAndUniformFlow
             obj.imSizeX = double(imageProperties.sizeX);
             obj.imSizeY = double(imageProperties.sizeY);
             obj.marginsY = double(imageProperties.marginsY);
-            obj.u = maxVelocityPixel*sqrt(2.0); 
-            obj.v = maxVelocityPixel*sqrt(2.0);
+	    obj.c1 = 0.35; %Ratio of constant velocity components u,v contribution for the maxVelocity magnitude 
+            if obj.c1 < 0.0 || obj.c1 > 1.0
+                error('Ratio c1 of constant velocity components u,v must be between 0.0 and 1.0');
+            end
+            obj.c = 1.0 - obj.c1;
+            obj.M = sqrt(obj.c^2+2.0*obj.c1^2+2.0*obj.c*obj.c1*sqrt(2.0));
+            obj.u = maxVelocityPixel*obj.c1/obj.M; 
+            obj.v = maxVelocityPixel*obj.c1/obj.M;
         end
         
         function [ x1, y1 ] = computeDisplacementAtImagePosition(obj, x0, y0 )
-            circulation = obj.maxVelocityPixel * 2.0 * obj.Radius;
+            circulation = obj.maxVelocityPixel * obj.c / obj.M;
             xc = obj.imSizeX/2.0;
             yc = obj.imSizeY/2.0;
 
             %Computed with Runge-Kutta numeric method of 4th order
-            h = obj.dt / 4.0;
+            h = obj.dt/1.0;
             %
             x1 = zeros(size(x0,1), size(x0,2));
             y1 = zeros(size(x0,1), size(x0,2));
@@ -62,47 +71,47 @@ classdef RankineVortexAndUniformFlow
                     yk1 = y0(i,j) - yc;
                     r = sqrt(xk1.^2 + yk1.^2);                    
                     if r > obj.Radius
-                        m = circulation;
+                        m = circulation * obj.Radius;
                         
-                        k1x = - h * ((m * yk1)/(xk1^2 + yk1^2) + obj.u);
+                        k1x = h * (-(m * yk1)/(xk1^2 + yk1^2) + obj.u);
                         k1y = h * ((m * xk1)/(xk1^2 + yk1^2) + obj.v);
 
                         xk2 = xk1 + k1x/2;
                         yk2 = yk1 + k1y/2;
-                        k2x = - h * ((m * yk2)/(xk2^2 + yk2^2) + obj.u);
+                        k2x = h * (-(m * yk2)/(xk2^2 + yk2^2) + obj.u);
                         k2y = h * ((m * xk2)/(xk2^2 + yk2^2) + obj.v);
 
                         xk3 = xk1 + k2x/2;
                         yk3 = yk1 + k2y/2;
-                        k3x = - h * ((m * yk3)/(xk3^2 + yk3^2) + obj.u);
+                        k3x = h * (-(m * yk3)/(xk3^2 + yk3^2) + obj.u);
                         k3y = h * ((m * xk3)/(xk3^2 + yk3^2) + obj.v);
 
                         xk4 = xk1 + k3x;
                         yk4 = yk1 + k3y;
-                        k4x = - h * ((m * yk4)/(xk4^2 + yk4^2) + obj.u);
+                        k4x = h * (-(m * yk4)/(xk4^2 + yk4^2) + obj.u);
                         k4y = h * ((m * xk4)/(xk4^2 + yk4^2) + obj.v);
 
                         x1(i,j) = xk1 + 1/6*(k1x + 2*k2x + 2*k3x + k4x) + xc;
                         y1(i,j) = yk1 + 1/6*(k1y + 2*k2y + 2*k3y + k4y) + yc;
                     else
-                        m = circulation / obj.Radius.^2;
+                        m = circulation/obj.Radius;
                         
-                        k1x = - h * ((m * yk1) + obj.u);
+                        k1x = h * (-(m * yk1) + obj.u);
                         k1y = h * ((m * xk1) + obj.v);
 
                         xk2 = xk1 + k1x/2;
                         yk2 = yk1 + k1y/2;
-                        k2x = - h * ((m * yk2) + obj.u);
+                        k2x = h * (-(m * yk2) + obj.u);
                         k2y = h * ((m * xk2) + obj.v);
 
                         xk3 = xk1 + k2x/2;
                         yk3 = yk1 + k2y/2;
-                        k3x = - h * ((m * yk3) + obj.u);
+                        k3x = h * (-(m * yk3) + obj.u);
                         k3y = h * ((m * xk3) + obj.v);
 
                         xk4 = xk1 + k3x;
                         yk4 = yk1 + k3y;
-                        k4x = - h * ((m * yk4) + obj.u);
+                        k4x = h * (-(m * yk4) + obj.u);
                         k4y = h * ((m * xk4) + obj.v);
 
                         x1(i,j) = xk1 + 1/6*(k1x + 2*k2x + 2*k3x + k4x) + xc;
